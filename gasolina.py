e11e0646956bc8047da799fde29fafe25dd59f1f
Matheus-Aureliano <MATHEUS252019@GMAIL.COM>
2025-07-02 13:24:18 +0000

    adicionando arquivos gasolina.csv/gasolina.py

---
+++ gasolina.py
@@ -0,0 +1,7 @@
+import seaborn as sns
+import pandas as pandas
+gasolina = pd.read_csv('gasolina.csv', sep;',')
+with sns.axes_style('whitegrid'):
+  grafico = sns.lineplot(x='dia', y='venda', data=gasolina)
+  grafico.set(title='Preço da gasolina nos primeiros dias de julho/2021', xlabel='Dias', ylabel='Preço R$');
+  grafico.get_figure().savefig('gasolina.png')
