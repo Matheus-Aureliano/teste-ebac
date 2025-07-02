23319e8325dc4dd2532f65ac49501d90469bf430
Matheus-Aureliano <MATHEUS252019@GMAIL.COM>
2025-07-02 13:31:46 +0000

    corrigindo erro do arquivo

--- gasolina.py
+++ gasolina.py
@@ -1,6 +1,6 @@
 import seaborn as sns
 import pandas as pandas
-gasolina = pd.read_csv('gasolina.csv', sep;',')
+gasolina = pd.read_csv('gasolina.csv', sep:',')
 with sns.axes_style('whitegrid'):
   grafico = sns.lineplot(x='dia', y='venda', data=gasolina)
   grafico.set(title='Preço da gasolina nos primeiros dias de julho/2021', xlabel='Dias', ylabel='Preço R$');
